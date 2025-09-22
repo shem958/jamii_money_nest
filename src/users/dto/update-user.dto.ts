4961705841ec88848ccfcd56d3389d57b05967a7
shem958 <shemgikunda37@gmail.com>
2025-09-22 12:55:23 +0300

    Remove unnecessary comment from UpdateUserDto to improve code clarity

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-// Makes every field from CreateUserDto optional for updates
 export class UpdateUserDto extends PartialType(CreateUserDto) { }
