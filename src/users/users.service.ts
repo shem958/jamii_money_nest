f3fc532743ac8adc308a2131c1677c6480ce1f46
Shem <108344120+shem958@users.noreply.github.com>
2025-09-22 20:02:55 +0300

    Updated package-lock.json

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../common/prisma.service';
 import { User } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
