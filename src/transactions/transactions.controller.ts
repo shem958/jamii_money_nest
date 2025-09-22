24447bb03eee2b70e62708019f44fdc3e3dc565f
shem958 <shemgikunda37@gmail.com>
2025-09-22 13:25:07 +0300

    Implement TransactionsController with CRUD endpoints for transaction management

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body } from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+    constructor(private readonly transactionsService: TransactionsService) { }
+
+    @Post()
+    create(@Body() dto: CreateTransactionDto) {
+        return this.transactionsService.create(dto);
+    }
+
+    @Get()
+    findAll() {
+        return this.transactionsService.findAll();
+    }
+
+    @Get('user/:userId')
+    findByUser(@Param('userId') userId: string) {
+        return this.transactionsService.findByUser(userId);
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.transactionsService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateTransactionDto) {
+        return this.transactionsService.update(id, dto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.transactionsService.remove(id);
+    }
+}
